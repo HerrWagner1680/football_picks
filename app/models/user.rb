68d7078a4f7e5a4bcd27e394481ccca8a7283a54
Sidney Wagner <herrwagner1680@gmail.com>
2015-11-23 00:16:03 -0500

    removed set primary key in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	set_primary_key :id
+	#set_primary_key :id
 	has_secure_password
 	#attr_accessible :user_name, :email, :password, :password_digest
 	has_many :picks

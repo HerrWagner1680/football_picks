6f7e12305dcbbca6093ac85e723eb0d10cf41734
Sidney Wagner <herrwagner1680@gmail.com>
2015-11-23 00:30:26 -0500

    changed model again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	#set_primary_key :id
 	has_secure_password
-	attr_accessible :user_name, :email, :password, :password_digest, :created_at, :updated_at
+	#attr_accessible :user_name, :email, :password, :password_digest
 	has_many :picks
 end

cb3fecd63681e93e3551070a53403f3fc0c839db
Sidney Wagner <herrwagner1680@gmail.com>
2015-11-23 00:27:32 -0500

    added attr to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	#set_primary_key :id
 	has_secure_password
-	#attr_accessible :user_name, :email, :password, :password_digest
+	attr_accessible :user_name, :email, :password, :password_digest, :created_at, :updated_at
 	has_many :picks
 end

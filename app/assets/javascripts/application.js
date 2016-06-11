51bbcde3c254f4ddff56ac88da2e14faf8decab8
Sidney Wagner <herrwagner1680@gmail.com>
2016-06-10 22:52:49 -0400

    isolating event listener to standings page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -204,35 +204,36 @@ hidePC = function(bool) {
     document.getElementById('horiz_overflow_standing').addEventListener("touchcancel", touchHandler, true);
 } */
 
+if ($('#horiz_overflow_standing').length) {
+      document.addEventListener("mousemove", function(){
+          event.preventDefault() ;
+          //showCoords();
+          if ($('#horiz_overflow_standing').length) {
+          document.getElementById('horiz_overflow_standing').innerHTML = "touchmove";
+          }
+      });
 
-document.addEventListener("mousemove", function(){
-    event.preventDefault() ;
-    //showCoords();
-    if ($('#testing_stuff').length) {
-    document.getElementById('testing_stuff').innerHTML = "touchmove";
-    }
-});
+      document.addEventListener("touchmove", function(){
+          event.preventDefault() ;
+          //showCoords();
+          if ($('#horiz_overflow_standing').length) {
+          document.getElementById('horiz_overflow_standing').innerHTML = "move move move";
+          }
+      });
 
-document.addEventListener("touchmove", function(){
-    event.preventDefault() ;
-    //showCoords();
-    if ($('#testing_stuff').length) {
-    document.getElementById('testing_stuff').innerHTML = "move move move";
-    }
-});
+      document.addEventListener("click", function(){
+          event.preventDefault() ;
+          //showCoords();
+          if ($('#horiz_overflow_standing').length) {
+          document.getElementById('horiz_overflow_standing').innerHTML = "sdhjksdfkhjsfkhjdfskjh";
+          }
+      }); 
 
-document.addEventListener("click", function(){
-    event.preventDefault() ;
-    //showCoords();
-    if ($('#testing_stuff').length) {
-    document.getElementById('testing_stuff').innerHTML = "sdhjksdfkhjsfkhjdfskjh";
-    }
-}); 
-
-document.addEventListener("touchend", function(){
-    event.preventDefault() ;
-    //showCoords();
-    if ($('#testing_stuff').length) {
-    document.getElementById('testing_stuff').innerHTML = "sdhjksdtouchendfskjh";
-    }
-}); 
+      document.addEventListener("touchend", function(){
+          event.preventDefault() ;
+          //showCoords();
+          if ($('#horiz_overflow_standing').length) {
+          document.getElementById('horiz_overflow_standing').innerHTML = "sdhjksdtouchendfskjh";
+          }
+      }); 
+};
